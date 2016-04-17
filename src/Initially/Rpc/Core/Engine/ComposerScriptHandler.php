040bb5dd7a42428ba7aacd972089fe5d5ce8ff42
陈文章 <381266902@qq.com>
2016-04-17 18:43:59 +0800

    composer update script handler

--- src/Initially/Rpc/Core/Engine/ComposerScriptHandler.php
+++ src/Initially/Rpc/Core/Engine/ComposerScriptHandler.php
@@ -1,15 +1,28 @@
 <?php
 namespace Initially\Rpc\Core\Engine;
 
+use Composer\Script\Event;
+use Initially\Rpc\Core\Config\Factory as ConfigFactory;
+use Initially\Rpc\Core\Config\Loader as ConfigLoader;
+use Initially\Rpc\Core\Support\Util;
+use Initially\Rpc\Proxy\Builder as ProxyBuilder;
+
 class ComposerScriptHandler
 {
 
     /**
      * Rebuild client service proxy
      */
-    public static function rebuildClientServiceProxy()
+    public static function rebuildClientServiceProxy(Event $event)
     {
-
+        if (null !== ($fileInfo = Util::getClientConfigFileInfo())) {
+            ConfigLoader::loadClient($fileInfo["file"]);
+            $clientConfigs = ConfigFactory::getClientAll();
+            $proxyBuilder = new ProxyBuilder();
+            foreach ($clientConfigs as $config) {
+                $proxyBuilder->createProxy($config->getInterface());
+            }
+        }
     }
 
 }
